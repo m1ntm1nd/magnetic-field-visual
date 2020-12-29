96aba11b512d3a2b6ef98aad0dc36e7e71f93077
fuse21 <kulagin.pavel.unn@gmail.com>
2020-12-29 15:27:35 +0300

    main file in ready

--- lol.py
+++ lol.py
@@ -2,11 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+def set_size(size):
+    plt.figure(dpi=150)
+    plt.xlim(-(size), (size))
+    plt.ylim(-(size), (size))
+    plt.grid()
 
-u = np.linspace(-1,1,100)
-x,y = np.meshgrid(u,u)
-z = x**2 + y**2
-ax.contour(x,y,z)
+
+def calculate_b(power, amperage = 1, length = 1):
+    return power / amperage / length
+
+
+def calc_len(x,y,x1,y1):
+    return np.sqrt((x-x1)**2+(y-y1)**2)
+
+x_axes = np.linspace(-10,10,10000)
+y_axes = np.linspace(-10,10,10000)
+cnt = 0
+list3 = [] 
+for i in x_axes:
+    list3.append([x_axes[cnt],y_axes[cnt],calc_len(x_axes[cnt],y_axes[cnt],0,0)])
+    cnt+=1
+arr = np.array(list3)
+print(arr)
+
+set_size(10)
+for i in arr:
+    if round(i[2],1) == 14.1:
+        plt.plot(i[0],i[1])
 plt.show()
+
---
+++ ready/fin.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x, y = np.mgrid[0:10:1000j, 0:10:1000j]
+
+#Формулы
+x_1, x_2, y_0 = 4, 6, 5
+y_2 = 6
+d = x_2 - x_1
+r_1 = np.sqrt((x - x_1)**2 + (y - y_0)**2)
+r_2 = np.sqrt((x - x_2)**2 + (y - y_2)**2)
+a = (d**2 - r_1**2 - r_2**2)/(2*r_1*r_2)
+B_1 = 1/(2*np.pi*(r_1))
+B_2 = (1/(2*np.pi*(r_2)))
+B = 10*np.sqrt(B_1**2 + B_2**2 + 2*B_1*B_2*np.cos(a))
+
+
+fig, axes = plt.subplots()
+lev = [1, 2, 3, 4, 6, 10, 20, 40, 100, 900]
+
+#  Контуры разного цвета:
+color_line = np.zeros((10, 3))
+color_line[:, 1:] = 0.1
+color_line[:, 0] = np.linspace(0, 1, 10)
+
+
+axes.contour(x, y, B, levels = lev,
+           colors = color_line)
+axes.set_title('Линии магнитного поля параллельных проводников в плоскости:')
+
+fig.set_figwidth(12)    
+fig.set_figheight(6)  
+plt.show()
